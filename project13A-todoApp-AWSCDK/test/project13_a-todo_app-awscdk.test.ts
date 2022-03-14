47595b2b06aa56384dc7bf7d22b2aed8d762ba9d
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-03-14 16:31:54 -0700

    initialized

---
+++ project13A-todoApp-AWSCDK/test/project13_a-todo_app-awscdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Project13ATodoAppAwscdk from '../lib/project13_a-todo_app-awscdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/project13_a-todo_app-awscdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Project13ATodoAppAwscdk.Project13ATodoAppAwscdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
