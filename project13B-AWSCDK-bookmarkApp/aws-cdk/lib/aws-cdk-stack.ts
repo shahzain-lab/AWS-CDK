c7bebee842379d8c29033409cc32e5b3d3b473bb
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-03-20 18:40:24 -0700

    in finalized shape

--- project13B-AWSCDK-bookmarkApp/aws-cdk/lib/aws-cdk-stack.ts
+++ project13B-AWSCDK-bookmarkApp/aws-cdk/lib/aws-cdk-stack.ts
@@ -1,11 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import * as appsync from '@aws-cdk/aws-appsync-alpha';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //************Frontend Deployment******************************* */
+
+    // Storage bucket for frontend assets
+    const websiteBucket = new s3.Bucket(this, 'website-storage-bucket', {
+      publicReadAccess: true,
+      versioned: true,
+      websiteIndexDocument: 'index.html',
+    })
+
+    // CDN distribution from origin
+    const distribution = new cloudfront.Distribution(this, 'dist', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket)
+      }
+    })
+
+    // Print domain to console
+    new cdk.CfnOutput(this, 'print-domainName', {
+      value: distribution.domainName
+    })
+
+    // deploying bucket
+    new s3deploy.BucketDeployment(this, 'bucketDeploy', {
+      sources: [s3deploy.Source.asset(join('__dirname', '/../../', 'packages/client/public'))],
+      destinationBucket: websiteBucket,
+      distribution: distribution
+    })
+
+    //************Backend API******************** */
 
     // Appsync Intialized
     const api = new appsync.GraphqlApi(this, 'myGraphqlAPI', {
